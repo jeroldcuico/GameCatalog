b87838117692298f005a18b9c08cedda8870f6c8
jeroldcuico <loverold17@gmail.com>
2025-04-20 14:15:19 -0400

    Fixes on Slug params

--- app/api/games/[slug]/route.ts
+++ app/api/games/[slug]/route.ts
@@ -3,8 +3,8 @@
 import { fetchData } from "@/lib/api";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
-  const { slug } = params;
+export async function GET(req: NextRequest, context: { params: { slug: string } }) {
+  const { slug } = context.params; // Correctly destructure the `slug` from the `params` object
 
   // Fetch data using the fetchData function
   const data = await fetchData(`games/${slug}`);
