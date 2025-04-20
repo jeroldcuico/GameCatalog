d56c87b4316d4d2f8c9d3d3b8d46ab246bbec99d
jeroldcuico <loverold17@gmail.com>
2025-04-20 14:11:27 -0400

    Update game slug fix

--- app/api/games/[slug]/route.ts
+++ app/api/games/[slug]/route.ts
@@ -1,7 +1,14 @@
+// app/api/games/[slug]/route.ts
+
 import { fetchData } from "@/lib/api";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: { slug: string } }) {
   const { slug } = params;
-  return fetchData(`games/${slug}`);
+
+  // Fetch data using the fetchData function
+  const data = await fetchData(`games/${slug}`);
+
+  // Return the fetched data as JSON
+  return NextResponse.json(data);
 }
